db752e041563183bdd8465a6459d143e0486ff9f
AlexeyBuryanov <alexeyburyanov@gmail.com>
2019-01-04 20:55:23 +0300

    Create UnitOfWork interface

---
+++ Clocks.Clients/Clocks.Clients/Clocks.Clients/Models/Database/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Clocks.Clients.Core.Models.Database.Repositories;
+
+namespace Clocks.Clients.Core.Models.Database
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICityRepository Cities { get; }
+        IClockTypeRepository ClockTypes { get; }
+        IClockRepository Clocks { get; }
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
+    }
+}
