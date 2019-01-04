9c183f3f9f3d59d7a2f0c8e1c408cc9c9fe1d3b9
AlexeyBuryanov <alexeyburyanov@gmail.com>
2019-01-04 20:54:53 +0300

    Update repositories

--- Clocks.Clients/Clocks.Clients/Clocks.Clients/Models/Database/Repositories/ClockRepository.cs
+++ Clocks.Clients/Clocks.Clients/Clocks.Clients/Models/Database/Repositories/ClockRepository.cs
@@ -1,78 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Clocks.Clients.Core.Models.Database.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Clocks.Clients.Core.Models.Database.Repositories
 {
-    public class ClockRepository : IRepository<Clock>, IRepositoryAsync<Clock>
+    public class ClockRepository : RepositoryAsync<Clock>, IClockRepository
     {
-        private readonly ApplicationDbContext _db;
-
-        public ClockRepository(ApplicationDbContext db)
+        public ClockRepository(ApplicationDbContext db) 
+            : base(db)
         {
-            _db = db;
         }
 
-        public List<Clock> GetAll()
-        {
-            return _db.Clocks
-                .Include(c => c.City)
-                .Include(c => c.ClockType)
-                .ToList();
-        }
+        public ApplicationDbContext ApplicationDbContext
+            => DbContext as ApplicationDbContext;
 
-        public Clock Get(int id)
+        public Task<List<Clock>> GetAllWithIncludesAsync()
         {
-            return _db.Clocks.Find(id);
-        }
-
-        public void Add(Clock item)
-        {
-            _db.Clocks.Add(item);
-        }
-
-        public void Update(Clock item)
-        {
-            _db.Entry(item).State = EntityState.Modified;
-        }
-
-        public void Delete(int id)
-        {
-            var clock = _db.Clocks.Find(id);
-            if (clock != null)
-                _db.Clocks.Remove(clock);
-        }
-
-
-        public Task<List<Clock>> GetAllAsync()
-        {
-            return _db.Clocks
+            return ApplicationDbContext.Clocks
                 .Include(c => c.City)
                 .Include(c => c.ClockType)
                 .ToListAsync();
-        }
-
-        public Task<Clock> GetAsync(int id)
-        {
-            return _db.Clocks.FindAsync(id);
-        }
-
-        public async Task AddAsync(Clock item)
-        {
-            await _db.Clocks.AddAsync(item);
-        }
-
-        public async Task DeleteAsync(int id)
-        {
-            var clock = await _db.Clocks.FindAsync(id);
-            if (clock != null)
-                _db.Clocks.Remove(clock);
-        }
-
-        public Task<bool> AnyAsync()
-        {
-            return _db.Clocks.AnyAsync();
         }
     }
 }
--- Clocks.Clients/Clocks.Clients/Clocks.Clients/Models/Database/Repositories/ClockTypeRepository.cs
+++ Clocks.Clients/Clocks.Clients/Clocks.Clients/Models/Database/Repositories/ClockTypeRepository.cs
@@ -1,72 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Clocks.Clients.Core.Models.Database.Repositories.Base;
 
 namespace Clocks.Clients.Core.Models.Database.Repositories
 {
-    public class ClockTypeRepository : IRepository<ClockType>, IRepositoryAsync<ClockType>
+    public class ClockTypeRepository : RepositoryAsync<ClockType>, IClockTypeRepository
     {
-        private readonly ApplicationDbContext _db;
-
-        public ClockTypeRepository(ApplicationDbContext db)
+        public ClockTypeRepository(ApplicationDbContext db) 
+            : base(db)
         {
-            _db = db;
         }
 
-        public List<ClockType> GetAll()
-        {
-            return _db.ClockTypes.ToList();
-        }
-
-        public ClockType Get(int id)
-        {
-            return _db.ClockTypes.Find(id);
-        }
-
-        public void Add(ClockType item)
-        {
-            _db.ClockTypes.Add(item);
-        }
-
-        public void Update(ClockType item)
-        {
-            _db.Entry(item).State = EntityState.Modified;
-        }
-
-        public void Delete(int id)
-        {
-            var type = _db.ClockTypes.Find(id);
-            if (type != null)
-                _db.ClockTypes.Remove(type);
-        }
-
-
-        public Task<List<ClockType>> GetAllAsync()
-        {
-            return _db.ClockTypes.ToListAsync();
-        }
-
-        public Task<ClockType> GetAsync(int id)
-        {
-            return _db.ClockTypes.FindAsync(id);
-        }
-
-        public async Task AddAsync(ClockType item)
-        {
-            await _db.ClockTypes.AddAsync(item);
-        }
-
-        public async Task DeleteAsync(int id)
-        {
-            var clockType = await _db.ClockTypes.FindAsync(id);
-            if (clockType != null)
-                _db.ClockTypes.Remove(clockType);
-        }
-
-        public Task<bool> AnyAsync()
-        {
-            return _db.ClockTypes.AnyAsync();
-        }
+        public ApplicationDbContext ApplicationDbContext 
+            => DbContext as ApplicationDbContext;
     }
 }
