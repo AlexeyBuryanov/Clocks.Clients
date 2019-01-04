ef08cef79f801f108cffdb0ed2f94ac292842370
AlexeyBuryanov <alexeyburyanov@gmail.com>
2019-01-04 20:48:15 +0300

    Create new interfaces repository

---
+++ Clocks.Clients/Clocks.Clients/Clocks.Clients/Models/Database/Repositories/Base/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Clocks.Clients.Core.Models.Database.Repositories.Base
+{
+    public interface IRepository<T> where T : class
+    {
+        T Get(int id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+        
+        void Add(T item);
+        void AddRange(IEnumerable<T> items);
+
+        void Update(T item);
+        void UpdateRange(IEnumerable<T> items);
+
+        void Delete(T item);
+        void DeleteRange(IEnumerable<T> items);
+
+        bool Any();
+    }
+}
---
+++ Clocks.Clients/Clocks.Clients/Clocks.Clients/Models/Database/Repositories/Base/IRepositoryAsync.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Clocks.Clients.Core.Models.Database.Repositories.Base
+{
+    public interface IRepositoryAsync<T> where T : class
+    {
+        Task<T> GetAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task<List<T>> FindAsync(Expression<Func<T, bool>> predicate);
+
+        Task AddAsync(T item);
+        Task AddRangeAsync(IEnumerable<T> items);
+
+        Task UpdateAsync(T item);
+        Task UpdateRangeAsync(IEnumerable<T> items);
+
+        Task DeleteAsync(T item);
+        Task DeleteRangeAsync(IEnumerable<T> items);
+
+        Task<bool> AnyAsync();
+    }
+}
