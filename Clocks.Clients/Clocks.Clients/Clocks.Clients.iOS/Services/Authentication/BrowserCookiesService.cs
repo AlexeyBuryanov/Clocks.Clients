150cabe5552ef448f70d5884818929fcc1bc33c1
AlexeyBuryanov <alexeyburyanov@gmail.com>
2019-02-01 19:13:47 +0300

    Add BrowserCookiesService in iOS project

---
+++ Clocks.Clients/Clocks.Clients/Clocks.Clients.iOS/Services/Authentication/BrowserCookiesService.cs
@@ -0,0 +1,21 @@
+﻿using Clocks.Clients.Core.Services.Authentication;
+using Foundation;
+using System.Threading.Tasks;
+
+namespace Clocks.Clients.iOS.Services
+{
+    public class BrowserCookiesService : IBrowserCookiesService
+    {
+        public Task ClearCookiesAsync()
+        {
+            var storage = NSHttpCookieStorage.SharedStorage;
+
+            foreach (var cookie in storage.Cookies)
+            {
+                storage.DeleteCookie(cookie);
+            }
+
+            return Task.FromResult(true);
+        }
+    }
+}
