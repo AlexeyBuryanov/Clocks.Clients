9ab446b43f17cbac43d81534571c09a8470ba82e
AlexeyBuryanov <alexeyburyanov@gmail.com>
2019-02-01 19:14:18 +0300

    Add DismissKeyboardService in iOS project

---
+++ Clocks.Clients/Clocks.Clients/Clocks.Clients.iOS/Services/DismissKeyboard/DismissKeyboardService.cs
@@ -0,0 +1,22 @@
+﻿using Clocks.Clients.Core.Services.DismissKeyboard;
+using Clocks.Clients.iOS.Services;
+using UIKit;
+
+[assembly: Xamarin.Forms.Dependency(typeof(DismissKeyboardService))]
+namespace Clocks.Clients.iOS.Services
+{
+    public class DismissKeyboardService : IDismissKeyboardService
+    {
+        public void DismissKeyboard() => UIApplication.SharedApplication.InvokeOnMainThread(() =>
+        {
+            var window = UIApplication.SharedApplication.KeyWindow;
+            var vc = window.RootViewController;
+            while (vc.PresentedViewController != null)
+            {
+                vc = vc.PresentedViewController;
+            }
+
+            vc.View.EndEditing(true);
+        });
+    }
+}
