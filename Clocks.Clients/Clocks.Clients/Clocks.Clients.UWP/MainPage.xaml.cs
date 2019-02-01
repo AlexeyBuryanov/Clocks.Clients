86806d22aa96f12570e900d0f341290b87b96e32
AlexeyBuryanov <alexeyburyanov@gmail.com>
2019-02-01 19:18:15 +0300

    Register DismissKeyboardService in UWP project

--- Clocks.Clients/Clocks.Clients/Clocks.Clients.UWP/MainPage.xaml.cs
+++ Clocks.Clients/Clocks.Clients/Clocks.Clients.UWP/MainPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Clocks.Clients.Core.Models.Database;
+using Clocks.Clients.Core.Services.DismissKeyboard;
 using Clocks.Clients.UWP.Database;
+using Clocks.Clients.UWP.Services.DismissKeyboard;
 using Prism;
 using Prism.Ioc;
 using CoreApp = Clocks.Clients.Core.App;
@@ -24,6 +26,7 @@ namespace Clocks.Clients.UWP
         public void RegisterTypes(IContainerRegistry containerRegistry)
         {
             containerRegistry.RegisterSingleton(typeof(IPath), typeof(UwpDbPath));
+            containerRegistry.RegisterSingleton(typeof(IDismissKeyboardService), typeof(DismissKeyboardService));
         }
     }
 }
